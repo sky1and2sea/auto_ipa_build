41ae7ccd7adf85a2a222f26cea98f509e8a180e8
sky1and2sea <sky1and2sea@aliyun.com>
2021-12-29 16:41:44 +0800

    第一次打包不需要输入管理密码

--- build_src/python/ipa-build.py
+++ build_src/python/ipa-build.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__author__ = 'Wu Xiaocheng'
+__author__ = 'liyanjun'
 
 '''
 编译xcode工程并生成ipa文件
@@ -23,17 +23,24 @@ if __name__ == '__main__':
     p12Pwd = sys.argv[5]
     mobileprovisionUrl = sys.argv[6]
     projectName = sys.argv[7]
+    bundleId = sys.argv[8]
+    p12FileUrlName = sys.argv[9]
+    
     params = None
     cleanCommand = None
     buildCommand = None
     createIPACommand = None
     importCommand = None
+    importPartitionListCommand = None
     exportCommand = None
     ipaPath = None
+    result = None
+    message = None
     print '----------python 开始-------------'
-
+    print 'buildConfig===',buildConfig
     print 'teamIdentifier %s' % teamIdentifier
     print 'provisioningProfile %s' % provisioningProfile
+    print mode
     currentPath = os.getcwd()
     if mode == 'debug':
         params = buildConfig.debug
@@ -45,70 +52,79 @@ if __name__ == '__main__':
             params.sdk, params.configuration, params.project, ipaPath)
     elif mode == 'release':
         params = buildConfig.release
-        ipaPath = '%s/appbuild/%s_%s_pro_%s' % (
-            currentPath,params.project, params.version, datetime.datetime.now().strftime("%m%d%H%M"))
-        importCommand = 'security import %s -P "%s"' % (
+        ipaPath = '%s/appbuild/%s_pro' % (
+            currentPath,params.project)
+        # importCommand = 'security import %s -P "%s" -A -T /usr/bin/codesign' % (
+        #     p12FileUrl,p12Pwd)
+        importCommand = 'security import %s -k ~/Library/Keychains/login.keychain -P "%s" -T /usr/bin/codesign -T /usr/bin/security -T /usr/bin/productbuild' % (
             p12FileUrl,p12Pwd)
+
+        importPartitionListCommand = 'security set-key-partition-list -S apple-tool:,apple: -s -k handhand ~/Library/Keychains/login.keychain 1> /dev/null'
+
+
         importMobileprovisionCommand = 'cp %s ~/Library/MobileDevice/Provisioning\ Profiles/%s.mobileprovision' % (
             mobileprovisionUrl,provisioningProfile)
-        importEntitlementsCommand = 'cp %s/build_src/push.entitlements %s/platforms/ios/push.entitlements' % (
-            currentPath,currentPath)		
-        buildCommand = 'xcodebuild -workspace platforms/ios/%s.xcworkspace -scheme %s  -sdk %s archive -configuration %s -destination generic/platform=iOS -archivePath appBuild/archive.xcarchive DEVELOPMENT_TEAM="%s" PROVISIONING_PROFILE="%s" CODE_SIGN_IDENTITY="iPhone Distribution" CODE_SIGN_ENTITLEMENTS="push.entitlements"' % (
-            projectName, projectName, params.sdk, params.configuration,teamIdentifier,provisioningProfile)        
+        # importEntitlementsCommand = 'cp %s/build_src/push.entitlements %s/platforms/ios/push.entitlements' % (
+            # currentPath,currentPath)      
+        #buildCommand = 'xcodebuild -workspace ../platforms/ios/%s.xcworkspace -scheme %s  -sdk %s archive -configuration %s -destination generic/platform=iOS -archivePath appBuild/archive.xcarchive DEVELOPMENT_TEAM="%s" PROVISIONING_PROFILE="%s" CODE_SIGN_IDENTITY="iPhone Distribution" CODE_SIGN_ENTITLEMENTS="push.entitlements"' % (
+            #projectName, projectName, params.sdk, params.configuration,teamIdentifier,provisioningProfile)
+        buildCommand = 'xcodebuild -workspace ../%s.xcworkspace -scheme %s  -sdk %s archive -configuration %s -destination generic/platform=iOS -archivePath appBuild/archive.xcarchive DEVELOPMENT_TEAM="%s" PROVISIONING_PROFILE_SPECIFIER="%s"  PRODUCT_BUNDLE_IDENTIFIER="%s"  CODE_SIGN_IDENTITY="iPhone Distribution"' % (
+            projectName, projectName, params.sdk, params.configuration,teamIdentifier,provisioningProfile,bundleId)
+        print buildCommand      
         #buildCommand = 'xcodebuild -project platforms/ios/%s.xcodeproj -scheme %s -sdk %s archive -configuration %s -destination generic/platform=iOS -archivePath appBuild/archive.xcarchive DEVELOPMENT_TEAM="%s" PROVISIONING_PROFILE="%s" CODE_SIGN_IDENTITY="iPhone Distribution" CODE_SIGN_ENTITLEMENTS="push.entitlements"' % (
             #params.xcworkspace, params.scheme, params.sdk, params.configuration,teamIdentifier,provisioningProfile)
-        exportCommand = 'xcodebuild -exportArchive -archivePath appBuild/archive.xcarchive -exportPath %s -exportOptionsPlist build_src/exportOptionsPlist.plist PROVISIONING_PROFILE="%s"' % (
+        exportCommand = 'xcodebuild -exportArchive -archivePath appBuild/archive.xcarchive -exportPath %s -exportOptionsPlist build_src/exportOptionsPlist.plist PROVISIONING_PROFILE="%s" -allowProvisioningUpdates' % (
             ipaPath,provisioningProfile)
-		
-			
+        
+    # ----------------------
 
-	"""
-    projectPath = params.path
-    currentPath = os.getcwd()
+    print '----xcode clean----'
+    if mode != 'debug':
+        xcodebuildclean = 'xcodebuild clean -workspace ../%s.xcworkspace -scheme %s -configuration Release' % (projectName,projectName)
+        print 'importCommand %s' % xcodebuildclean
+        result, message = commands.getstatusoutput(xcodebuildclean)
 
-    os.chdir(projectPath)
-    print 'switchToPath %s' % os.getcwd()
+        if result != 0:
+            print message
+        else:
+            print "----xcode clean success ------"
 
-    print 'clean project'
-	
-    cleanCommand = 'xcodebuild -target %s clean' % params.target
-    result, message = commands.getstatusoutput(cleanCommand)
-	"""
-	
 
-	# ----------------------
-	print 'importCommand %s' % importCommand
+    if mode != 'debug':
+        unlockChain = 'security unlock-keychain -p handhand ~/Library/Keychains/login.keychain'
+        commands.getstatusoutput(unlockChain)
+        print 'unlockChain:',unlockChain
 
-	result, message = commands.getstatusoutput(importCommand)
+        # commands.getstatusoutput(importCommand)
+        print 'importCommand %s' % importCommand
+        result, message = commands.getstatusoutput(importCommand)
 
-    if result != 0:
-        print message
-        raise ShellError('shell error occur command is %s' % importCommand)
-    else:
-        print ">> importCommand success"
-		
-	# ----------------------
-	print 'importMobileprovisionCommand %s' % importMobileprovisionCommand
+        if result != 0:
+            print message
+        else:
+            print ">> importCommand success"
 
-	result, message = commands.getstatusoutput(importMobileprovisionCommand)
+
+        print 'importPartitionListCommand %s' % importPartitionListCommand
+        result2, message2 = commands.getstatusoutput(importPartitionListCommand)
+
+        if result2 != 0:
+            print message2
+        else:
+            print ">> importPartitionListCommand success"
+
+    # ----------------------
+
+        
+    print 'importMobileprovisionCommand %s' % importMobileprovisionCommand
+    result, message = commands.getstatusoutput(importMobileprovisionCommand)
 
     if result != 0:
         print message
         raise ShellError('shell error occur command is %s' % importMobileprovisionCommand)
     else:
-        print ">>importMobileprovisionCommand success"		
+        print ">>importMobileprovisionCommand success"      
 
-    #-----------------------------
-
-	print 'importEntitlementsCommand %s' % importEntitlementsCommand
-
-	result, message = commands.getstatusoutput(importEntitlementsCommand)
-
-    if result != 0:
-        print message
-        raise ShellError('shell error occur command is %s' % importEntitlementsCommand)
-    else:
-        print ">>importEntitlementsCommand success"		
     #-----------------------------
     print 'build project'
     print 'buildCommand %s' % buildCommand
@@ -120,8 +136,8 @@ if __name__ == '__main__':
     else:
         print ">>build success"
 
-		
-		
+        
+        
     print 'create ipa'
     print 'buildCommand %s' % exportCommand
 
@@ -144,5 +160,5 @@ if __name__ == '__main__':
         raise ShellError('shell error occur command is %s' % clearCommand)
     else:
         print "clear success"
-	"""
+    """
 
